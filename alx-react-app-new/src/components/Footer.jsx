53b07c508ba00d7a9685908b17f873271296f6fb
Charles Sawe <csawe14@gmail.com>
2025-11-07 06:38:30 +0300

    Added alx-react-app-new project

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div>
+            <footer style={{ backgroundColor: 'black', color: 'white', padding: '15px', textAlign: 'center', marginTop: '20px' }}>
+              <p style={{ fontSize: '14px' }}>© 2023 City Lovers</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+    return (
+        <div>
+            <main style={{ padding: '20px', backgroundColor: '#f2f2f2', textAlign: 'center' }}>
+              <p style={{ fontSize: '18px' }}>I love to visit New York, Paris, and Tokyo.</p>
+            </main>
+        </div>
+    );
+}
+
+export default MainContent;
