7960ffb7af121ce1d14a6993fbf0191b2ee66eda
Charles Sawe <csawe14@gmail.com>
2025-11-30 17:37:15 +0300

    Deploying github to vercel

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,17 +1,30 @@
 import axios from "axios";
 
+const api = axios.create({
+  baseURL: "https://api.github.com",
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
+  },
+});
+
 
 export const fetchUserData = async (username) => {
   try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
+    const response = await api.get(`/users/${username}`);
     return response.data;
   } catch (error) {
+    console.error("Error fetching user:", error);
     throw error;
   }
 };
 
 
-export const advancedUserSearch = async (username, location, minRepos, page = 1) => {
+export const advancedUserSearch = async (
+  username,
+  location,
+  minRepos,
+  page = 1
+) => {
   let query = "";
 
   if (username) query += `${username} in:login `;
@@ -19,11 +32,13 @@ export const advancedUserSearch = async (username, location, minRepos, page = 1)
   if (minRepos) query += `repos:>=${minRepos} `;
 
   try {
-    const response = await axios.get(
-      `https://api.github.com/search/users?q=${query}&page=${page}&per_page=30`
+    const response = await api.get(
+      `/search/users?q=${query}&page=${page}&per_page=30`
     );
+
     return response.data;
   } catch (error) {
+    console.error("Error in advanced search:", error);
     throw error;
   }
 };
