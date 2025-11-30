8cd46012f3afd85222a2cc9b9026c55bb237bda5
Charles Sawe <csawe14@gmail.com>
2025-11-30 09:41:01 +0300

    setting up github user research application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>GitHub User Search</h1>
+      <p> {import.meta.env.VITE_APP_GITHUB_API_KEY}</p>
+    </div>
+  );
+}
+
+export default App;
